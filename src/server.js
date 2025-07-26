b60be2a7b4bdb2fe33926ccc37617d3a767f5cc2
Maryna <tromsa3122@mail.com>
2025-07-26 17:52:20 +0300

    add router creators

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino';
+import creatorsRouter from './routers/creators.js';
 
 const logger = pino({
-    transport: {
-        target: "pino-pretty",
-    },
+  transport: {
+    target: 'pino-pretty',
+  },
 });
 
 const app = express();
@@ -14,22 +15,23 @@ app.use(cors());
 app.use(express.json());
 // app.use('/authors');
 // app.use('/articles');
+app.use('/api/creators', creatorsRouter);
 
 app.use((req, res) => {
-    res.status(404).json({
-        message: 'Not Found',
-    });
+  res.status(404).json({
+    message: 'Not Found',
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 
 const setupServer = async () => {
-return new Promise((resolve) => {
+  return new Promise((resolve) => {
     app.listen(PORT, () => {
-        logger.info(`Server is running on port ${PORT}`);
-        resolve(app);
+      logger.info(`Server is running on port ${PORT}`);
+      resolve(app);
     });
-});
+  });
 };
 
 export default setupServer;
