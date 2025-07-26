12fd5cbf4607c29dca24c4352b1e3380010a1c26
Ivane <ivanmitj@gmail.com>
2025-07-26 13:00:51 +0200

    add middleware

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,1 +1,9 @@
-// check if id is valid
+import createError from 'http-errors';
+import mongoose from 'mongoose';
+
+export const isValidId = (req, res, next) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        next(createError(400, 'Invalid ID'));
+    }
+    next();
+}; 
