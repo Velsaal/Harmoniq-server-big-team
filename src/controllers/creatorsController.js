0c2abd9ac30007f08fdefe139fa6a55a66bfe639
Ivane <ivanmitj@gmail.com>
2025-08-05 11:48:10 +0200

    all creators fix

--- src/controllers/creatorsController.js
+++ src/controllers/creatorsController.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js';
+import Article from '../models/Article.js';
 
 export const getTopCreators = async (req, res, next) => {
   try {
@@ -11,8 +12,20 @@ export const getTopCreators = async (req, res, next) => {
 
 export const getAllCreators = async (req, res, next) => {
   try {
-    const creators = await User.find({}, 'name avatarUrl articlesAmount email createdAt updatedAt');
-    res.json(creators);
+    const creators = await User.find({}, 'name avatarUrl email');
+    const creatorsWithArticlesAmount = await Promise.all(
+      creators.map(async (creator) => {
+        const articlesAmount = await Article.countDocuments({ ownerId: creator._id });
+        return {
+          _id: creator._id,
+          name: creator.name,
+          avatarUrl: creator.avatarUrl,
+          email: creator.email,
+          articlesAmount,
+        };
+      })
+    );
+    res.json(creatorsWithArticlesAmount);
   } catch (error) {
     next(error);
   }
