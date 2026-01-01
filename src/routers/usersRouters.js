e75941ddc078297c093d061bf37f1a40043a31d9
yuliia levytska <lupin773311asd@gmail.com>
2026-01-01 21:23:07 +0000

    дюд

--- src/routers/usersRouters.js
+++ src/routers/usersRouters.js
@@ -1,25 +1,58 @@
 import { Router } from "express";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
-import upload from "../middlewares/upload.js";
+import { upload } from "../middlewares/multer.js";
+
 import {
   getUserInfo,
   getSavedArticles,
   addArticleToSaved,
   removeArticleFromSaved,
   updateUserInfo,
-  uploadUserAvatar
+  uploadUserAvatar,
 } from "../controllers/userController.js";
 
 const userRouter = Router();
 
+/* ===== Saved articles ===== */
+userRouter.get(
+  "/:userId/saved-articles",
+  authMiddleware,
+  ctrlWrapper(getSavedArticles)
+);
 
-userRouter.get('/:userId', ctrlWrapper(getUserInfo));
-userRouter.get('/:userId/saved-articles', ctrlWrapper(getSavedArticles));
-userRouter.post('/:userId/saved-articles/:articleId', ctrlWrapper(addArticleToSaved));
-userRouter.delete('/:userId/saved-articles/:articleId', ctrlWrapper(removeArticleFromSaved));
-userRouter.patch('/:userId', ctrlWrapper(updateUserInfo));
-userRouter.post('/:userId/avatar', upload.single('avatar'), ctrlWrapper(uploadUserAvatar));
+userRouter.post(
+  "/:userId/saved-articles/:articleId",
+  authMiddleware,
+  ctrlWrapper(addArticleToSaved)
+);
 
+userRouter.delete(
+  "/:userId/saved-articles/:articleId",
+  authMiddleware,
+  ctrlWrapper(removeArticleFromSaved)
+);
+
+/* ===== Avatar ===== */
+userRouter.post(
+  "/:userId/avatar",
+  authMiddleware,
+  upload.single("avatar"),
+  ctrlWrapper(uploadUserAvatar)
+);
+
+/* ===== Update user ===== */
+userRouter.patch(
+  "/:userId",
+  authMiddleware,
+  ctrlWrapper(updateUserInfo)
+);
+
+/* ===== Get user info (ВСЕГДА В КОНЦЕ) ===== */
+userRouter.get(
+  "/:userId",
+  authMiddleware,
+  ctrlWrapper(getUserInfo)
+);
 
 export default userRouter;
